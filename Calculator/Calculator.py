5d1bf02a023ecabe1a28f474f86c7a720e81e730
Hirvita Mandaviya <113379704+Hirvita6@users.noreply.github.com>
2024-05-30 02:19:08 +0530

    Update Calculator.py

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -19,32 +19,38 @@ def Modulus(x,y):
     except ZeroDivisionError:
         return "can't divide by zero"
 
-print("Simple Calculator")
-print("1 for Addition")
-print("2 for Subtraction")
-print("3 for Multiplication")
-print("4 for Division")
-print("5 for Modulus")
+def main():
 
-choice = int(input("Enter your Choice(1/2/3/4/5) : "))
-num1 = float(input("Enter the first number : "))
-num2 = float(input("Enter the second number : "))
+    print("Simple Calculator")
+    print("1 for Addition")
+    print("2 for Subtraction")
+    print("3 for Multiplication")
+    print("4 for Division")
+    print("5 for Modulus")
 
-match choice:
-    case 1:
-        result = Addition(num1, num2)
-    case 2:
-        result = Subtraction(num1, num2)
-    case 3:
-        result = Multiplication(num1, num2)
-    case 4:
-        result = Division(num1, num2)
-    case 5:
-        result = Modulus(num1, num2)
-    case _:
+    choice = int(input("Enter your Choice(1/2/3/4/5) : "))
+    if choice in range(1,6):
+        num1 = float(input("Enter the first number : "))
+        num2 = float(input("Enter the second number : "))
+
+        match choice:
+            case 1:
+                result = Addition(num1, num2)
+            case 2:
+                result = Subtraction(num1, num2)
+            case 3:
+                result = Multiplication(num1, num2)
+            case 4:
+                result = Division(num1, num2)
+            case 5:
+                result = Modulus(num1, num2)
+            case _:
+                print("Invalid Choice input")
+
+        print(f"Result: {result}")
+    else:
         print("Invalid Choice input")
 
-print(f"Result: {result}")
-
-
+if __name__=='__main__':
+    main()
 
